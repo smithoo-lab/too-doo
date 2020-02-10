cc2f4f9d4ac000d1011590cb98386ce8180a9e4e
smithoo <smithoo7646@gmail.com>
2020-02-11 01:24:05 +0900

    feat: add home page style

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,9 +1,22 @@
 <template>
-    <div>Home</div>
+    <div class="home">
+        <p>We have too much work to doo.</p>
+    </div>
 </template>
 
-<script>
-export default {
-
-};
-</script>
+<style scoped>
+.home {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #faffed;
+}
+.home p {
+    font-size: 24px;
+    color: #555;
+}
+</style>
